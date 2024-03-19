ba19bf67b590a332de7791d124285e8b129f1a28
alanqrwang <alanqrwang@gmail.com>
2024-03-19 09:59:35 -0400

    added fast_dice from synthseg

--- keymorph/loss_ops.py
+++ keymorph/loss_ops.py
@@ -413,3 +413,48 @@ class HardDicePairwiseLoss(torch.nn.Module):
                     loss += self.hard_dice(img1, img2)[0]
                     num += 1
         return loss / num
+
+
+def fast_dice(x, y, labels):
+    """Fast implementation of Dice scores.
+    :param x: input label map
+    :param y: input label map of the same size as x
+    :param labels: numpy array of labels to evaluate on
+    :return: numpy array with Dice scores in the same order as labels.
+    """
+
+    assert (
+        x.shape == y.shape
+    ), "both inputs should have same size, had {} and {}".format(x.shape, y.shape)
+
+    if len(labels) > 1:
+        # sort labels
+        labels_sorted = np.sort(labels)
+
+        # build bins for histograms
+        label_edges = np.sort(
+            np.concatenate([labels_sorted - 0.1, labels_sorted + 0.1])
+        )
+        label_edges = np.insert(
+            label_edges,
+            [0, len(label_edges)],
+            [labels_sorted[0] - 0.1, labels_sorted[-1] + 0.1],
+        )
+
+        # compute Dice and re-arrange scores in initial order
+        hst = np.histogram2d(x.flatten(), y.flatten(), bins=label_edges)[0]
+        idx = np.arange(start=1, stop=2 * len(labels_sorted), step=2)
+        dice_score = (
+            2 * np.diag(hst)[idx] / (np.sum(hst, 0)[idx] + np.sum(hst, 1)[idx] + 1e-5)
+        )
+        dice_score = dice_score[np.searchsorted(labels_sorted, labels)]
+
+    else:
+        dice_score = dice(x == labels[0], y == labels[0])
+
+    return dice_score
+
+
+def dice(x, y):
+    """Implementation of dice scores for 0/1 numpy array"""
+    return 2 * np.sum(x * y) / (np.sum(x) + np.sum(y))
