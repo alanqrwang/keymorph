81a9a8889a850c2a4a2c3ce18f6fb0fcab2a1f19
alanqrwang <alanqrwang@gmail.com>
2024-07-02 11:51:52 -0400

    updated versioning

--- keymorph/__init__.py
+++ keymorph/__init__.py
@@ -1,4 +1,4 @@
-__version__ = "2.0.0"
+__version__ = "2.0.1"
 __author__ = "Alan Q. Wang"
 
 from . import augmentation
--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="keymorph",
-    version="2.0.0",
+    version="2.0.1",
     author="Alan Q. Wang",
     author_email="alanqrwang@gmail.com",
     url="https://github.com/alanqrwang/keymorph",
